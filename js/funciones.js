713962cf867de2a3c3951fb993fa4f87c8577458
lucasbernalb <lucasbernalb@gmail.com>
2024-07-25 19:00:17 -0300

    agregamos propiedad cantidad y la sumamos al localstorage

--- js/funciones.js
+++ js/funciones.js
@@ -1,12 +1,12 @@
 const productos = [
-    {id:1 , nombre:"Pesadillas Nocturnas", precio:2000, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/cuadro%20negro.jpg", categoria:"cuadros"},
-    {id:2 , nombre:"Desurbanismo", precio:2500, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/desurbanismo.jpg", categoria:"cuadros"},
-    {id:3 , nombre:"Cielo y Tierra", precio:2500, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/entre%20el%20cielo%20y%20la%20tierra.jpg", categoria:"cuadros"},
-    {id:4 , nombre:"Condor", precio:2000, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/mi%20condor.jpg", categoria:"cuadros"},
-    {id:5 , nombre:"Todo lo que existe", precio:3000, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/todo%20lo%20que%20existe.jpg", categoria:"cuadros"},
-    {id:6 , nombre:"Mi sol", precio:2000, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/mujer%20en%20ventada.jpg", categoria:"cuadros"},
-    {id:7 , nombre:"Cenizas", precio:2500, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/mujer%20y%20fuego.jpg", categoria:"cuadros"},
-    {id:8 , nombre:"Parte del aire", precio:4000, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros//parte%20del%20aire.jpg", categoria:"cuadros"},
+    {id:1 , nombre:"Pesadillas Nocturnas", precio:2000, cantidad:1, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/cuadro%20negro.jpg", categoria:"cuadros"},
+    {id:2 , nombre:"Desurbanismo", precio:2500, cantidad:1, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/desurbanismo.jpg", categoria:"cuadros"},
+    {id:3 , nombre:"Cielo y Tierra", precio:2500, cantidad:1, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/entre%20el%20cielo%20y%20la%20tierra.jpg", categoria:"cuadros"},
+    {id:4 , nombre:"Condor", precio:2000, cantidad:1, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/mi%20condor.jpg", categoria:"cuadros"},
+    {id:5 , nombre:"Todo lo que existe", precio:3000, cantidad:1, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/todo%20lo%20que%20existe.jpg", categoria:"cuadros"},
+    {id:6 , nombre:"Mi sol", precio:2000, cantidad:1, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/mujer%20en%20ventada.jpg", categoria:"cuadros"},
+    {id:7 , nombre:"Cenizas", precio:2500, cantidad:1, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros/mujer%20y%20fuego.jpg", categoria:"cuadros"},
+    {id:8 , nombre:"Parte del aire", precio:4000, cantidad:1, imagen:"https://lucasbernalb.github.io/PreEntrega.Bernal/img/productos/cuadros//parte%20del%20aire.jpg", categoria:"cuadros"},
 ]
 
 function renderProductos(productos) {
@@ -29,14 +29,28 @@ function renderProductos(productos) {
 
 function agregarProductos(id) {
     const producto = productos.find(item => item.id == id);
-    
     const carrito = cargarCarritoLocalS();
-    
+
     carrito.push(producto);
     guardarCarritoLS(carrito);
     renderBotonCarrito();
+    
+    if (carrito.some((item) => item.id == id)){
+        carrito = carrito.map((cantidadProductoCarrito) => {
+            if (cantidadProductoCarrito.id == id) {
+                return { ...cantidadProductoCarrito, cantidad: cantidadProductoCarrito.cantidad++}
+            }
+            return guardarCarritoLS(carrito);
+    
+        })
+    
+    }
+
     console.log(`El producto ${producto.nombre} se ha agregado correctamente!`);
-}
+    }
+
+
+
 
 function renderBotonCarrito() {
     let total = totalProductos();
@@ -50,7 +64,7 @@ function eliminarCarritoLS(id) {
     guardarCarritoLS(carritoActualizado);
     renderCarrito();
     renderBotonCarrito();
-    sumarProductosCarrito();git 
+    sumarProductosCarrito();
     console.log("El producto #" + id + " ha sido removido correctamente"); 
 }
 
@@ -83,8 +97,9 @@ function cargarProductoLS() {
     return producto;
 }
 
+
 function sumarProductosCarrito() {
-        let precio = JSON.parse(localStorage.getItem("carrito"));
+        let precio = cargarCarritoLocalS();
         const productoPrecio = precio.map(item => item.precio);
         let productoPrecioF = 0;
         productoPrecioF = productoPrecio.reduce((acumulador, numero) => acumulador + numero, 0);
